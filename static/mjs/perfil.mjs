529cc9341c8193c83ceda08483a8a586c9878453
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2024-09-24 18:25:07 -0300

    Refactor CSS files: Adjust margins and font family

--- static/mjs/perfil.mjs
+++ static/mjs/perfil.mjs
@@ -28,32 +28,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
             if(imc < 18.5){
 
-                document.getElementById('imc').style.color = 'red';
                 mensagem = 'Abaixo do peso';
 
             } else if(imc >= 18.5 && imc < 24.9){
 
-                document.getElementById('imc').style.color = 'green';
                 mensagem = 'Peso normal';
 
             } else if(imc >= 25 && imc < 29.9){
 
-                document.getElementById('imc').style.color = 'orange';
                 mensagem = 'Sobrepeso';
 
             } else if(imc >= 30 && imc < 34.9){
 
-                document.getElementById('imc').style.color = 'red';
                 mensagem = 'Obesidade grau 1';
 
             } else if(imc >= 35 && imc < 39.9){
 
-                document.getElementById('imc').style.color = 'red';
                 mensagem = 'Obesidade grau 2';
 
             } else if(imc >= 40){
 
-                document.getElementById('imc').style.color = 'red';
                 mensagem = 'Obesidade grau 3';
 
             }
