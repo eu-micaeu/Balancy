00c4208b6ac4923d09aa9819dfa2932cd25fb2e4
eu-micaeu <micaelrrocha13@gmail.com>
2024-10-06 01:26:01 -0300

    Refactor CSS files: Adjust margins, font family, and button styling

--- static/mjs/perfil.mjs
+++ static/mjs/perfil.mjs
@@ -26,61 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('height').value = data.height + ' m';
             document.getElementById('activityLevel').value = data.activity_level;
 
-            // Cálculo do IMC (Índice de Massa Corporal)
-            // Cálculo do IMC (Índice de Massa Corporal)
-            var imc = data.weight / (data.height * data.height);
-            var mensagem = '';
 
-            if (imc < 18.5) {
-                mensagem = 'Abaixo do peso';
-            } else if (imc >= 18.5 && imc < 24.9) {
-                mensagem = 'Peso normal';
-            } else if (imc >= 25 && imc < 29.9) {
-                mensagem = 'Sobrepeso';
-            } else if (imc >= 30 && imc < 34.9) {
-                mensagem = 'Obesidade grau 1';
-            } else if (imc >= 35 && imc < 39.9) {
-                mensagem = 'Obesidade grau 2';
-            } else if (imc >= 40) {
-                mensagem = 'Obesidade grau 3';
-            }
-
-            document.getElementById('imcValue').textContent = 'IMC: ' + imc.toFixed(2) + ' - ' + mensagem;
-
-            // Cálculo da TMB (Taxa de Metabolismo Basal)
-            // TMB é a quantidade de calorias que o corpo queima em repouso total (sem considerar atividades físicas).
-            var tmb;
-            var weight = data.weight;
-            var height = data.height * 100; // Convertendo altura para cm
-            var age = data.age;
-            var gender = data.gender;
-
-            if (gender === 'M') {
-                tmb = 88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age);
-            } else if (gender === 'F') {
-                tmb = 447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age);
-            }
-
-            document.getElementById('tmb').textContent = 'TMB (Taxa de Metabolismo Basal): ' + tmb.toFixed(2) + ' kCal';
-
-            // Cálculo do TMR (Taxa Metabólica de Repouso)
-            // TMR é a quantidade de calorias que o corpo queima considerando o nível de atividade física.
-            var tmr;
-            switch (data.activity_level) {
-                case 'sedentary':
-                    tmr = tmb * 1.2;
-                    break;
-                case 'lightly_active':
-                    tmr = tmb * 1.55;
-                    break;
-                case 'moderately_active':
-                    tmr = tmb * 1.9;
-                    break;
-                default:
-                    alert('Erro ao calcular TMR');
-            }
-
-            document.getElementById('tmr').textContent = 'TMR (Taxa Metabólica de Repouso): ' + tmr.toFixed(2) + ' kCal';
 
         })
         .catch(error => console.error('Error:', error));
