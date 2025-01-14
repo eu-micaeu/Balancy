349c2507e9bed42a6394a3bcce140bb1ba39a501
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2025-01-14 19:42:21 -0300

    Remove Dockerfile, docker-compose.yml, logo.svg, and clean up main.go

--- server/main.go
+++ server/main.go
@@ -1,40 +1,37 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-
-	"github.com/eu-micaeu/Balancy/server/database"
-	"github.com/eu-micaeu/Balancy/server/middlewares"
-	"github.com/eu-micaeu/Balancy/server/routes"
+    "github.com/gin-gonic/gin"
+    "github.com/eu-micaeu/Balancy/server/database"
+    "github.com/eu-micaeu/Balancy/server/middlewares"
+    "github.com/eu-micaeu/Balancy/server/routes"
 )
 
 func main() {
-	r := gin.Default() // Cria o servidor Gin
+    r := gin.Default()
 
-	// Middleware CORS
-	r.Use(middlewares.CorsMiddleware())
+    // Middlewares
+    r.Use(middlewares.CorsMiddleware())
+    r.Use(middlewares.CacheCleanerMiddleware())
 
-	// Middleware de limpeza de cache
-	r.Use(middlewares.CacheCleanerMiddleware())
+    // Conecta ao banco de dados
+    db, err := database.NewDB()
+    if err != nil {
+        panic(err)
+    }
 
-	// Conecta ao banco de dados
-	db, err := database.NewDB()
-	if err != nil {
-		panic(err)
-	}
+    // Rotas do backend
+    routes.UserRoutes(r, db)
+    routes.MenuRoutes(r, db)
+    routes.MealRoutes(r, db)
+    routes.FoodRoutes(r, db)
 
-	// Define as rotas da aplicação
-	routes.UserRoutes(r, db)
-	routes.MenuRoutes(r, db)
-	routes.MealRoutes(r, db)
-	routes.FoodRoutes(r, db)
+    // Rota para validação de token
+    r.GET("/validateToken", middlewares.ValidarTokenHandler)
 
-	// Rota para validação de token
-	r.GET("/validateToken", middlewares.ValidarTokenHandler)
-
-	// Executa o servidor
-	err = r.Run()
-	if err != nil {
-		panic(err)
-	}
+    // Inicia o servidor
+    err = r.Run()
+    if err != nil {
+        panic(err)
+    }
 }
