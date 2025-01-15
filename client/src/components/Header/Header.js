4c5b51e86d520740967f28a645745d3df625c289
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2025-01-15 19:12:49 -0300

    Update login API URL in Header component to use HTTPS

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -53,7 +53,7 @@ function Header() {
 
     const handleLogin = async () => {
         try {
-            const response = await fetch('http://localhost:10000/login', {
+            const response = await fetch('https://localhost:10000/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
