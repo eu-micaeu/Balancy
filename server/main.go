70f3c23a11d877cf0d6fe773af3cf67f5bc3c9a4
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2025-01-15 19:01:32 -0300

    Update routes to serve React frontend and static files in main server file

--- server/main.go
+++ server/main.go
@@ -1,46 +1,44 @@
 package main
 
 import (
-    "net/http"
-    "github.com/eu-micaeu/Balancy/server/database"
-    "github.com/eu-micaeu/Balancy/server/middlewares"
-    "github.com/eu-micaeu/Balancy/server/routes"
-    "github.com/gin-gonic/gin"
+	"github.com/eu-micaeu/Balancy/server/database"
+	"github.com/eu-micaeu/Balancy/server/middlewares"
+	"github.com/eu-micaeu/Balancy/server/routes"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-    // Middlewares
-    r.Use(middlewares.CorsMiddleware())
-    r.Use(middlewares.CacheCleanerMiddleware())
+	// Middlewares
+	r.Use(middlewares.CorsMiddleware())
+	r.Use(middlewares.CacheCleanerMiddleware())
 
-    // Conecta ao banco de dados
-    db, err := database.NewDB()
-    if err != nil {
-        panic(err)
-    }
+	// Conecta ao banco de dados
+	db, err := database.NewDB()
+	if err != nil {
+		panic(err)
+	}
 
-    // Rotas do backend
-    routes.UserRoutes(r, db)
-    routes.MenuRoutes(r, db)
-    routes.MealRoutes(r, db)
-    routes.FoodRoutes(r, db)
+	// Rotas do backend
+	routes.UserRoutes(r, db)
+	routes.MenuRoutes(r, db)
+	routes.MealRoutes(r, db)
+	routes.FoodRoutes(r, db)
 
-    // Rota para validação de token
-    r.GET("/validateToken", middlewares.ValidarTokenHandler)
+	// Rota para validação de token
+	r.GET("/validateToken", middlewares.ValidarTokenHandler)
 
-    // Rota para servir o frontend React
-    r.StaticFS("/app", http.Dir("./build"))
+	// Rota para servir todo o React (Front-End)
+	r.StaticFile("/", "../client/build/index.html")
+	r.Static("/static", "../client/build/static")
 
-    // Rota para lidar com outras rotas que não existam (fallback para index.html)
-    r.NoRoute(func(c *gin.Context) {
-        c.File("./build/index.html")
-    })
+	// Logo
+	r.StaticFile("/logoPrincipal.png", "../client/build/logoPrincipal.png")
 
-    // Inicia o servidor
-    err = r.Run()
-    if err != nil {
-        panic(err)
-    }
+	// Inicia o servidor
+	err = r.Run()
+	if err != nil {
+		panic(err)
+	}
 }
