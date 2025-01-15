3cb184f7238d605a35acfd3ce124ce411cd828d5
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2025-01-15 19:29:52 -0300

    Update login API URL and asset manifest; remove obsolete license file

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -53,7 +53,7 @@ function Header() {
 
     const handleLogin = async () => {
         try {
-            const response = await fetch('https://localhost:10000/login', {
+            const response = await fetch('https://balancy-c6v4.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
